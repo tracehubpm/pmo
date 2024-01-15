731d3aef69030bed01bcc8627f525a52930a26c8
hizmailovich <theannalios@gmail.com>
2024-01-15 15:22:29 +0300

    Initial commit

---
+++ src/main/java/git/tracehub/pmo/PmoApplication.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023-2024 Tracehub
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package git.tracehub.pmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point.
+ *
+ * @since 0.0.0
+ */
+@SpringBootApplication
+public class PmoApplication {
+
+	/**
+	 * Application entry point.
+	 *
+	 * @param args Application arguments
+	 */
+	public static void main(final String[] args) {
+		SpringApplication.run(PmoApplication.class, args);
+	}
+
+}
