e5b418c2609a920d3a11471d781722f21f55f3f2
hizmailovich <theannalios@gmail.com>
2024-01-24 15:54:23 +0300

    Tests for expressions added

---
+++ src/test/java/git/tracehub/pmo/security/expression/Authentication.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2023-2024 Proxy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package git.tracehub.pmo.security.expression;
+
+import java.time.Instant;
+import lombok.RequiredArgsConstructor;
+import org.cactoos.map.MapEntry;
+import org.cactoos.map.MapOf;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+/**
+ * Authorized user.
+ *
+ * @since 0.0.0
+ */
+@RequiredArgsConstructor
+@SuppressWarnings({"JTCOP.RuleAllTestsHaveProductionClass", "JTCOP.RuleCorrectTestName"})
+public final class Authentication {
+
+    /**
+     * Email.
+     */
+    private final String email;
+
+    /**
+     * Authenticate user.
+     */
+    public void authenticate() {
+        final UsernamePasswordAuthenticationToken auth =
+            new UsernamePasswordAuthenticationToken(
+                new Jwt(
+                    "value",
+                    Instant.now(),
+                    Instant.MAX,
+                    new MapOf<>(
+                        new MapEntry<>("header", "value")
+                    ),
+                    new MapOf<>(
+                        new MapEntry<>("email", this.email)
+                    )
+                ),
+                null,
+                null
+            );
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+}
